08b3951797704cdd841f59cc1eaceaee42f8ed5e
Tugba Ekici <tugba.ekici@kuartis.com>
2023-12-11 10:47:42 +0300

    Up to authorization part

---
+++ app/dashboard/revenue-chart.tsx
@@ -0,0 +1,22 @@
+import { generateYAxis } from '@/app/lib/utils';
+import { CalendarIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
+ 
+// ...
+ 
+export default async function RevenueChart() { // Make component async, remove the props
+  const revenue = await fetchRevenue(); // Fetch data inside the component
+ 
+  const chartHeight = 350;
+  const { yAxisLabels, topLabel } = generateYAxis(revenue);
+ 
+  if (!revenue || revenue.length === 0) {
+    return <p className="mt-4 text-gray-400">No data available.</p>;
+  }
+ 
+  return (
+    <div>dd</div>
+  );
+}
+ 
